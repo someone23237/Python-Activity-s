3b231fcce821ef1e1d34d8433a681e26fcf2028b
Someone23237 <sudeepthecoolest@gmail.com>
2024-12-18 17:14:50 -0800

    Done

---
+++ diffandsamenumbers.py
@@ -0,0 +1,11 @@
+x=5
+if type(x)is int:
+    print("type(x)is a integer")
+else:
+    print("x is not a integer")
+x=5.0
+if type(x) is float:
+    print("type(x)is a float")
+else:
+    print("x is not a float")
+    
