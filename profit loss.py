25566bddb85e6408261ab6bb43f00a88df07795a
Someone23237 <sudeepthecoolest@gmail.com>
2024-12-18 16:50:46 -0800

    Done

--- profit loss.py
+++ profit loss.py
@@ -1,3 +1,7 @@
 sale_amount=float(input("Enter the number"))
 cash_price=float(input("Enter the number"))
-if sale_amount
+if sale_amount>cash_price:
+    print("this is a profit")
+else:
+    print("this is a loss")
+    
